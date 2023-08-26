11d1dd701e7c2b5d91622be5a4adb132000fbd3e
Leo Godin <leogodin217@gmail.com>
2023-08-26 09:05:55 -0400

    Add products and orders

---
+++ models/sources/products_base.sql
@@ -0,0 +1,25 @@
+ -- depends_on: {{ ref('fake_products') }}
+ -- depends_on: {{ ref('fake_dates') }}
+ -- depends_on: {{ ref('fake_numbers') }}
+
+{# No unique key, because this is append only #}
+{{
+  config(
+    materialized = 'incremental',
+    tags = ['base-table', 'companies']
+    )
+}}
+
+{% set max_per_day = var('max_per_day', 3) %}
+{% set allow_zero = var('allow_zero', True) %}
+{% set start_date = get_default_start_date() %}
+{% set end_date = get_default_end_date() %}
+
+{{ generate_source_model_query(seed_name='fake_products', 
+                               max_per_day = max_per_day, 
+                               allow_zero = allow_zero,
+                               start_date = start_date, 
+                               end_date = end_date, 
+                               columns=['id', 'category', 'name', 'price']
+   )
+}}
